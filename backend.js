a037d282c6c7234712056be595ec0d2db4390b2b
Elliot <liniguy@Oracle.local>
2023-01-21 21:30:27 -0800

    added delete function and find user by job

--- backend.js
+++ backend.js
@@ -42,8 +42,15 @@ app.get('/', (req, res) => {
 
 app.get('/users', (req, res) => {
     const name = req.query.name;
+    const job = req.query.job;
     if (name != undefined){
-        let result = findUserByName(name);
+        let result = {}
+        if (job != undefined) {
+            result = findUserByNameJob(name, job);
+        }
+        else {
+            result = findUserByName(name);
+        }
         result = {users_list: result};
         res.send(result);
     }
@@ -64,12 +71,15 @@ app.get('/users/:id', (req, res) => {
 });
 
 function findUserById(id) {
-    return users['users_list'].find( (user) => user['id'] === id); // or line below
-    //return users['users_list'].filter( (user) => user['id'] === id);
+    return users['users_list'].find( (user) => user['id'] === id); 
 }
 
 const findUserByName = (name) => { 
     return users['users_list'].filter( (user) => user['name'] === name); 
+}
+
+const findUserByNameJob = (name, job) => { 
+    return users['users_list'].filter( (user) => user['name'] === name && user['job'] === job); 
 }
 
 
@@ -82,6 +92,19 @@ app.post('/users', (req, res) => {
 function addUser(user){
     users['users_list'].push(user);
 }
+
+
+// FIND INDEX, USE SPLICE TO DELETE, FindIndex FUNCTION
+app.delete('/users/:id', (req, res) => {
+    const id = req.params['id'];
+    const idNum = users['users_list'].findIndex( (user) => user.id === id );
+    if (users['users_list'].at(idNum).id === id) {
+        users['users_list'].splice(idNum, 1);
+    }
+    res.status(200).end();
+});
+
+
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
